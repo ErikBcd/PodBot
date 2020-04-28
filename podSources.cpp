0abdd03edb600f8c42a9ab47ecf6f32fb7d5599b
Estugon <estugon-tem@gmx.de>
2020-04-28 04:04:13 +0200

    OwOfier ist jetzt noch besser!
    Reagiert auf "N"+Vokal und setzt an Satzzeichen noch die passenden Emotes ran.
    Es ist furchtbar.
    Es ist großartig.

--- podSources.cpp
+++ podSources.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <regex>
 #include "sleepy_discord/sleepy_discord.h"
 #include "sleepy_discord/websocketpp_websocket.h"
 using namespace std;
@@ -20,7 +21,7 @@ public:
 			string msg = message.content.substr( indicator.size(), message.content.size() - indicator.size());
 			cout << "MSG = " +msg << endl;
 			if (message.startsWith(indicator + " OwOfy!")) {
-				sendMessage(message.channelID, owofy(msg));
+				sendMessage(message.channelID, owofyRegEx(msg));
 			}
 			else if (message.startsWith(indicator + " Help") || message.startsWith(indicator + " help")) {
 				sendMessage(message.channelID, helpCommands());
@@ -31,8 +32,12 @@ public:
 			else if (message.startsWith(indicator + " Good Bot") || message.startsWith(indicator + "Good bot")) {
 				sendMessage(message.channelID, "Thank you kindly, " + message.author.username);
 			}
-			else if (message.startsWith(indicator + "Update Status") && message.author.username == "Estugon") {
+			else if (message.startsWith(indicator + " Update Status") && message.author.username == "Estugon") {
 				updateStatus(msg.substr(string(" Update Status ").size(), msg.size() - string(" Update Status ").size()));
+			}
+			else if (message.startsWith(indicator + " Goodbye") && message.author.username == "Estugon") {
+				sendMessage(message.channelID, "Okay, bye! o/");
+				this->quit();
 			}
 		}
 	}
@@ -72,5 +77,38 @@ public:
 		else {
 			return "No usable Text found UwU \\nUsage: " + indicator + " OwOfy! *Very cool Text*";
 		}
+	}
+
+	string owofyRegEx(string input) {
+		
+		input = input.substr(string(" OwOfy!").size(), input.size() - string(" OwOfy!").size());
+
+		regex w("(?:r|l)");
+		string output = input;
+		
+		output = regex_replace (input, w, "w");
+
+
+		regex upperCaseW("(?:R|L)");
+		output = regex_replace (output, upperCaseW, "W");
+
+
+		regex ny("n([aeiou])");
+		regex upperCaseNy("N([aeiou])");
+		regex upperCaseNY("N([AEIOU])");
+		regex ove("(ove)");
+		regex exclamation("\!");
+		regex question("\\?");
+		regex point("\\.");
+
+		output = regex_replace (output, ny, "ny$1");
+		output = regex_replace (output, upperCaseNy, "Ny$1");
+		output = regex_replace (output, upperCaseNY, "NY$1");
+		output = regex_replace(output, ove, "uv");
+		output = regex_replace(output, exclamation, "$& >w<");
+		output = regex_replace(output, question, "$& UwU");
+		output = regex_replace(output, point, "$& OwO");
+
+		return output;
 	}
 };
